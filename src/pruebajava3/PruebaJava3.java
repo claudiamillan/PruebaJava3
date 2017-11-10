c41f007e027fff7e6827218b73e4b7b78a317489
claudiamillan <millanc569@gmail.com>
2017-11-10 19:24:09 -0300

    Realizando una ventana JFrame2

--- src/pruebajava3/PruebaJava3.java
+++ src/pruebajava3/PruebaJava3.java
@@ -4,18 +4,29 @@
  * and open the template in the editor.
  */
 package pruebajava3;
-
+import interfaces.Interfaz;
+import javax.swing.JFrame;
 /**
- *
+ * Claudia Millan Lopez
  * @author lab12
+ * Fecha: 10 de Noviembre de 2017
+ * Programa: PruebaJava3
+ * Responsabilidad: Conexion de Escritorio
  */
-public class PruebaJava3 {
+public class PruebaJava3 
+{
 
     /**
      * @param args the command line arguments
      */
-    public static void main(String[] args) {
-        // TODO code application logic here
+    public static void main(String[] args) 
+    {
+    
+      Interfaz w = new Interfaz();
+      w.setVisible(true);
+      w.setBounds(0, 0, 400, 400);
+      w.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+      
     }
     
 }
